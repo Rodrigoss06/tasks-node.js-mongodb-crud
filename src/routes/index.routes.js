e5f05752955a42079cfbc581324b945399040334
Rodrigoss06 <102317296+Rodrigoss06@users.noreply.github.com>
2022-08-13 21:35:47 -0500

    first commit

---
+++ src/routes/index.routes.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import {
+  renderTask,
+  createTask,
+  About,
+  editDireccion,
+  editRequest,
+  Delete,
+  Done,
+} from "../controllers/task.controllers";
+
+const router = Router();
+
+router.get("/", renderTask);
+
+router.post("/tasks/add", createTask);
+
+router.get("/about", About);
+
+router.get("/edit/:id", editDireccion);
+
+router.post("/edit/:id", editRequest);
+
+router.get("/delete/:id", Delete);
+
+router.get("/toggleDone/:id", Done);
+
+export default router;
